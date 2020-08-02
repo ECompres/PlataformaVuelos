f50c250e625c8a4948142c228642f56b1defd086
Elias Compres <ecompres2017@gmail.com>
2020-08-02 17:08:33 -0400

    Deploy

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { PaisesServices } from './Services/paises.service';
 import { Paises } from './Models/paises';
+import { VuelosService } from './Services/vuelos.service';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -10,16 +11,36 @@ import { Paises } from './Models/paises';
 export class AppComponent {
   title = 'PlataformaVuelos';
 
-  
+
 
   public paises: any;
 
-  constructor(private paisesService: PaisesServices) {
+  constructor(private vuelosService: VuelosService) {
 
   }
-  
+
   ngOnInit(): void {
-    // this.getPaises();
+    let vuelosArr = [
+      {
+        id: 1,
+        pais: "Afganistan",
+        fechaHora: "2020-01-01T01:01",
+        tiempoVuelo: "3 horas",
+        codigo: "3D3HGR",
+        pasajeros: []
+      },
+      {
+        id: 2,
+        pais: "United States",
+        fechaHora: "2020-01-01T01:01",
+        tiempoVuelo: "3 horas",
+        codigo: "4KFJ43",
+        pasajeros: []
+      }
+    ]
+    localStorage.setItem('vuelos', JSON.stringify(vuelosArr));
+
+
 
   }
 
